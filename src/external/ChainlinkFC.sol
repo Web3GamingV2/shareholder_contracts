97b7299be3ff11ca603cafbc95da4e8ed52813e3
leelongxi <leelongxi@foxmail.com>
2025-04-14 21:42:33 +0800

    Init

--- src/external/ChainlinkFC.sol
+++ src/external/ChainlinkFC.sol
@@ -1,6 +1,7 @@
 // SPDX-License-Identifier: UNLICENSED
 pragma solidity ^0.8.13;
 
+// https://remix.ethereum.org/#url=https://docs.chain.link/samples/ChainlinkFunctions/GettingStartedFunctionsConsumer.sol&autoCompile=true&lang=en&optimize=false&runs=200&evmVersion=null&version=soljson-v0.8.19+commit.7dd6d404.js
 contract ChainlinkFC {
     
 }
