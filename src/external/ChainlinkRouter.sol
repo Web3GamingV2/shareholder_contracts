379e05a6617ce7481d0e3c916db6ba2fe61a7b4b
leelongxi <leelongxi@foxmail.com>
2025-04-28 12:14:55 +0800

    Init

---
+++ src/external/ChainlinkRouter.sol
@@ -0,0 +1,15 @@
+// SPDX-License-Identifier: UNLICENSED
+pragma solidity ^0.8.13;
+
+/**
+ * @title 链上路由
+ * @dev 处理链上的路由逻辑，包括路由请求和响应。
+ * 链上路由合约将接收链上的路由请求，并根据请求的参数进行处理。
+ * 1. VRF
+ * 2. FC
+ * 3. Sender/Receiver
+ * 4. Feeder
+ */
+contract ChainlinkRouter {
+
+}
