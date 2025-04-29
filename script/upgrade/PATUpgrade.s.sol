a066dc58003dcfc4d68a590e691096850c989f02
leelongxi <leelongxi@foxmail.com>
2025-04-29 16:37:24 +0800

    Init

--- script/upgrade/PATUpgrade.s.sol
+++ script/upgrade/PATUpgrade.s.sol
@@ -3,7 +3,8 @@ pragma solidity ^0.8.13;
 
 import {Script, console} from "forge-std/Script.sol";
 import {Upgrades} from "openzeppelin-foundry-upgrades/Upgrades.sol";
-import {PAT} from "../src/core/PAT.sol";
+import {PAT} from "../../src/core/PAT.sol";
+
 contract PATUpgrader is Script {
     function run() external returns (address) {
     
